ba5d8582c7ad871e705c9ade7648c65c1a71ae80
minhkakart <nguyenminh2732@gmail.com>
2023-09-30 13:20:56 +0700

    Them comment giai thich code

--- MyPerceptron.py
+++ MyPerceptron.py
@@ -2,43 +2,45 @@ from random import randint
 import numpy
 
 class MyPerceptron:
+
+    ## Hàm Khởi tạo
     def __init__(self, xTrain, yTrain) -> None:
-        self.lenXY = len(xTrain)
-        if self.lenXY != len(yTrain):
+        self.lenXY = len(xTrain)        ## Lưu số lượng bộ dữ liệu đầu vào
+        if self.lenXY != len(yTrain):           ## Nếu số lượng bộ dữ liệu đầu vào khác số lượng nhãn thì báo lỗi 
             raise ValueError('Parameters is not valid!')
-        self.X = numpy.array(xTrain)
-        self.Y = numpy.array([list(map(lambda x: 1 if x == 1 else -1, yTrain))]).T
-        self.w = []
+        self.X = numpy.array(xTrain)        ## Lưu tập dữ liệu đầu vào
+        self.Y = numpy.array([list(map(lambda x: 1 if x == 1 else -1, yTrain))]).T      ## Lưu tập nhãn dạng ma trận 1 cột
+        self.w = []         ## Khởi tạo bộ trọng số w
         for i in range(len(self.X[0])):
-            self.w.append(randint(int(-len(self.X[0])), int(len(self.X[0]))))
-        self.w = numpy.array([self.w])
+            self.w.append(randint(int(-len(self.X[0])), int(len(self.X[0]))))       ## Sinh ngẫu nhiên bộ trọng số w
+        self.w = numpy.array([self.w])      ## Lưu lại bộ trọng số dưới dạng ma trận hàng
     
+    ## Hàm kiểm tra xem nhãn được được đánh dấu đúng hay sai
     def check(self, x, y):
-        # print('check')
-        # print(numpy.array([x]).T@self.w)
-        a = numpy.array(x)
-        res = self.w@a
-        return True if res*y >= 0 else False
+        xi = numpy.array(x)                         ## Lưu xi
+        res = self.w@xi                             ## Tính nhãn dự đoán res = y^ = w*xi
+        return True if res*y >= 0 else False        ## Nếu res (giá trị dự đoán) * y (giá trị thực tế) >=0 (cùng phía) thì nhãn được gán đúng
 
+    ## Hàm kiểm tra xem toàn bộ nhãn đã được đánh dấu đúng chưa
     def stop(self):
-        # print('stop')
-        # print(enumerate(self.X))
-        for i, v in enumerate(self.X):
-            if not self.check(v, self.Y[i]):
-                return False, i
-        return True, -1
+        for i, v in enumerate(self.X):              ## Duyệt toàn bộ dữ liệu đầu vào
+            if not self.check(v, self.Y[i]):        ## Kiểm tra nếu gặp một bộ bị gán nhãn sai thì trả về False và vị trí bộ dữ liệu bị gán sai
+                return False, i         
+        return True, -1                             ## Chạy được đến đây tức là toàn bộ các bộ dữ liệu đã được gán nhãn đúng
 
-    def fit(self, eta = 0.001):
-        count = 0
-        while count < 1000:
-            check, index = self.stop()
-            if check:
+    ## Hàm train mô hình
+    def fit(self, eta = 0.001):                     
+        count = 0                                   ## Khởi tạo biến đếm số lần lặp
+        while count < 1000:                         ## Sử dụng thuật toán gradient descent để tính đạo hàm hàm mất mát = 0, số lần lặp là 1000
+            check, index = self.stop()              ## Lấy về giá trị kiểm tra và chỉ số từ hàm stop()
+            if check:                               ## Nếu check là True thì kết thúc
                 return
-            tmp = eta*(numpy.array([self.Y[index]])*numpy.array([self.X[index]]))
-            self.w = self.w + tmp
-            count += 1
-        print(count)
+            yixi = (numpy.array([self.Y[index]])*numpy.array([self.X[index]]))
+            self.w = self.w + eta*yixi              ## Cập nhật lại hàm mất mát với hệ số học eta mặc định = 0.001
+            count += 1                              ## Tăng biến đếm
+
+    ## Hàm dự đoán
     def predict(self, xTest):
-        test = numpy.array(xTest)
-        res = (test@self.w.T).T[0]
-        return list(map(lambda x: 0 if x < 0 else 1, res))
+        test = numpy.array(xTest)           ## Lưu tập dữ liệu test
+        res = (test@self.w.T).T[0]          ## Tính toán nhãn dự đoán của tập dữ liệu
+        return list(map(lambda x: 0 if x < 0 else 1, res))      ## Biến đổi tập nhãn dự đoán thành dạng số 0 1 rồi trả về
--- main.py
+++ main.py
@@ -6,26 +6,29 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
 try:
-    raw_data = pandas.read_csv('classification/cars.csv')
+    raw_data = pandas.read_csv('perceptron/mushrooms.csv')
 except:
-    raw_data = pandas.read_csv('cars.csv')
+    raw_data = pandas.read_csv('mushrooms.csv')
 
+## Đọc dữ liệu
 data = raw_data.apply(LabelEncoder().fit_transform)
-data = data[['buying','maint','doors','persons','lug_boot','safety','acceptability']].values
+## Loại bỏ tiêu đề
+data = data[['class','cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment','gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']].values
 
+## Tách dữ liệu 
 train, test = train_test_split(data, test_size=0.3, shuffle=False)
 
-xTrain = train[:,:6]
-yTrain = list(train[:,6])
-xTest = test[:,:6]
-yTest = list(test[:,6])
+## Nhãn là cột đầu tiên, bộ dữ liệu là 9 cột kế tiếp
+xTrain, yTrain = train[:,1:10], list(train[:,0]) ## Dữ liệu train
+xTest, yTest = test[:,1:10], list(test[:,0]) ## Dữ Liệu test
 
-a = MyPerceptron(xTrain, yTrain)
-a.fit(eta=0.01)
-yPredict = a.predict(xTest)
-count = 0
-for i, v in enumerate(yPredict):
-    if v == yTest[i]:
-        count += 1
+a = MyPerceptron(xTrain, yTrain)        ## Khởi tạo đối tượng
+a.fit(eta=0.01)                         ## Train mô hình với hệ số học eta = 0.01
+yPredict = a.predict(xTest)             ## Dự đoán tập test
+count = 0                               ## Tạo một biến đếm
+for i, v in enumerate(yPredict):        ###-------------------------###
+    if v == yTest[i]:                   ### Đếm số lần dự đoán đúng ###
+        count += 1                      ###-------------------------###
 
+## In kết quả
 print('Du doan dung {0}/{1} = {2}'.format(count, len(yTest), count/len(yTest)))
